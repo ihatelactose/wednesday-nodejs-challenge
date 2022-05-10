546088d55d85b61b3a852c57527c7937645d357a
Sourav <sourav@Apples-MacBook-Pro.local>
2022-05-10 15:53:26 +0530

    refactor: updated camel case to snake case in SQL files and updated bookings and users model with the same

--- resources/v2/01_bookings.sql
+++ resources/v2/01_bookings.sql
@@ -1,8 +1,8 @@
 CREATE TABLE bookings
   (
-    id            INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    fromLocation  VARCHAR(32) NOT NULL,
-    toLocation    VARCHAR(32) NOT NULL,
-    byUser        INT NOT NULL,
-    CONSTRAINT bookings_users_fk FOREIGN KEY (byUser) REFERENCES users (id) ON UPDATE CASCADE
+    id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    from_location   POINT NOT NULL SRID 4326,
+    to_location     POINT NOT NULL SRID 4326,
+    by_user         INT NOT NULL,
+    CONSTRAINT bookings_users_fk FOREIGN KEY (by_user) REFERENCES users (id) ON UPDATE CASCADE
   )
