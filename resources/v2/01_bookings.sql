92c69fdd8c33d21489765ba7c8960ef15482c203
Sourav <sourav@Apples-MacBook-Pro.local>
2022-05-11 10:05:12 +0530

    refactor: moved the insert query for booking from route handler to bookingDao

--- resources/v2/01_bookings.sql
+++ resources/v2/01_bookings.sql
@@ -4,5 +4,6 @@ CREATE TABLE bookings
     from_location   POINT NOT NULL SRID 4326,
     to_location     POINT NOT NULL SRID 4326,
     by_user         INT NOT NULL,
+    created_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
     CONSTRAINT bookings_users_fk FOREIGN KEY (by_user) REFERENCES users (id) ON UPDATE CASCADE
   )
