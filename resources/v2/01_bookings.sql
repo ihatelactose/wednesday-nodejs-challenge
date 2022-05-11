6b5e76705302c3cc77b5c8f279d4e31a9dfbdd61
Sourav <sourav@Apples-MacBook-Pro.local>
2022-05-11 17:22:29 +0530

    feat: Created updateCab, query for nearby cabs - updated migrations from POINT to GEOMETRY - updated cabDao, driverDao with find and update - added more routes to /booking, /cab, and /driver

--- resources/v2/01_bookings.sql
+++ resources/v2/01_bookings.sql
@@ -1,8 +1,8 @@
 CREATE TABLE bookings
   (
     id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    from_location   POINT NOT NULL SRID 4326,
-    to_location     POINT NOT NULL SRID 4326,
+    from_location   GEOMETRY NOT NULL,
+    to_location     GEOMETRY NOT NULL,
     user_id         INT NOT NULL,
     created_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
     CONSTRAINT bookings_users_fk FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE
--- resources/v3/01_drivers.sql
+++ resources/v3/01_drivers.sql
@@ -3,5 +3,5 @@ CREATE TABLE drivers
    id               INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
    first_name        VARCHAR(32) NOT NULL,
    last_name        VARCHAR(32) NOT NULL,
-   current_location POINT NOT NULL SRID 4326
+   current_location GEOMETRY NOT NULL
  )
