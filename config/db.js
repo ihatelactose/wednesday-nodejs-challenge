a1f5b43a97b0b9b9d6154c39a1ec866207b05d3a
ihatelactose <apple@Apples-MacBook-Pro.local>
2022-05-17 10:28:52 +0530

    feat: updated config/db with configurations for prod environment

--- config/db.js
+++ config/db.js
@@ -15,5 +15,22 @@ module.exports = {
             userscored: true,
             timestamps: false
         }
+    },
+    prod: {
+        url: process.env.MYSQL_ENDPOINT,
+        database: process.env.MYSQL_DATABASE,
+        password: process.env.MYSQL_MASTER_PASSWORD,
+        username: process.env.MYSQL_MASTER_USERNAME,
+        logging: true,
+        dialect: 'mysql',
+        pool: {
+            min: 0,
+            max: 10,
+            idle: 10000
+        },
+        define: {
+            userscored: true,
+            timestamps: false
+        }
     }
 };
